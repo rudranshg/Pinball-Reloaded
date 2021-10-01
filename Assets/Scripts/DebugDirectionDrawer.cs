2b976ba045dc3add7080d91c705bc4fe6fdf56c2
Rudransh Gupta <rudrag0101@gmail.com>
2021-10-01 18:16:44 +0530

    Adding Game

--- Assets/Scripts/DebugDirectionDrawer.cs
+++ Assets/Scripts/DebugDirectionDrawer.cs
@@ -1,12 +1,13 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DebugDirectionDrawer : MonoBehaviour
-{   public float length = 1f;
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position, transform.position + (transform.forward * length));
-    }
+// Quick test script to see what is forward on a object
+public class DebugDirectionDrawer : MonoBehaviour {
+	public float length = 1f;
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine(transform.position, transform.position + (transform.forward * length));
+	}
 }
